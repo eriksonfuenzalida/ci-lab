fb0bec9eabd61c8bab09ba06ab64fbcae4bcab51
eriksonfuenzalida <114364018+eriksonfuenzalida@users.noreply.github.com>
2023-10-30 21:08:54 -0300

    Update App.java codigo escaleno

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,11 +7,14 @@ public class App {
   
 
   public static String triangulo(int a, int b, int c) {
+    if (a + b <= c || b + c <= a || c + a <= b) {
+      return "error no es un triangulo";
+    }
     if (a == b && b != c && c != a){
     return "es un triangulo isoceles";
     }
-    if (a + b <= c || b + c <= a || c + a <= b) {
-      return "error no es un triangulo";
+    if(a != b && b != c && c != a){
+    return "es un triangulo escaleno";
     }
     if (a == b && b == c && c == a)
     {
