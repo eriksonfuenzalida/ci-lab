9cf90b088644f47349a25df862087c13f2065719
eriksonfuenzalida <114364018+eriksonfuenzalida@users.noreply.github.com>
2023-10-30 21:23:55 -0300

    Update App.java fix 0

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,17 +10,19 @@ public class App {
     if (a + b <= c || b + c <= a || c + a <= b) {
       return "error no es un triangulo";
     }
-    if (a == b && b != c && c != a){
-    return "es un triangulo isoceles";
-    }
-    if(a != b && b != c && c != a){
-    return "es un triangulo escaleno";
-    }
-    if (a == b && b == c && c == a){
-      return "es un triangulo equilatero";
-    }
-    if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a)){
-      return "es un triangulo pitagorico";
+    if (a > 0 && b > 0 && c > 0){
+      if (a == b && b != c && c != a){
+      return "es un triangulo isoceles";
+      }
+      if(a != b && b != c && c != a){
+      return "es un triangulo escaleno";
+      }
+      if (a == b && b == c && c == a){
+        return "es un triangulo equilatero";
+      }
+      if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a)){
+        return "es un triangulo pitagorico";
+      }
     }
     return "error no es un triangulo";
   } 
